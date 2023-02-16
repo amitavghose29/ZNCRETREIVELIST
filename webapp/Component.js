c39224a68710a0b3d7301f65260e96611df8f28f
Amitav GHOSE <amitav.ghose.external@airbus.com>
2023-02-16 07:00:14 +0000

    Nc Retreive

---
+++ webapp/Component.js
@@ -0,0 +1,13 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.airbus.zncretreivelist.Component", {
+            metadata: {
+                manifest: "json"
+            }
+           
+        });
+    }
+);
